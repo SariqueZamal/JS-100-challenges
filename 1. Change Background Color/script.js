1937f343649df3caaeda08e1b1254a216bf017af
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-10 21:45:56 +0300

    update code for bg color change

--- 1. Change Background Color/script.js
+++ 1. Change Background Color/script.js
@@ -1,2 +1,9 @@
 let button = document.querySelector("button");
 
+button.addEventListener("click",()=>{
+    let blue = Math.random * 256;
+    let green = Math.random * 256;
+    let red = Math.random * 256;
+
+    document.body.style.backgroundColor =  `rgb(${blue},${green},${red})`
+});
