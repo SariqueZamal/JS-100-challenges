bcf8601d0daffdb7af2b77501b7ad1e78a7cb210
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-10 21:55:57 +0300

    final update

--- 1. Change Background Color/script.js
+++ 1. Change Background Color/script.js
@@ -1,9 +1,11 @@
 let button = document.querySelector("button");
+let p = document.querySelector("p");
 
 button.addEventListener("click", function () {
   let blue = Math.floor(Math.random() * 256);
   let green = Math.floor(Math.random() * 256);
   let red = Math.floor(Math.random() * 256);
 
+  p.innerText = `Color Code: rgb(${blue},${green},${red})`
   document.body.style.backgroundColor = `rgb(${blue},${green},${red})`;
 });
