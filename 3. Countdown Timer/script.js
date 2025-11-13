2dc18efad31195c05366932c8bd4a92917366f7a
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-13 21:56:45 +0300

    initial logic in script file

--- 3. Countdown Timer/script.js
+++ 3. Countdown Timer/script.js
@@ -3,3 +3,10 @@ let days = document.querySelector(".days");
 let hours = document.querySelector(".hours");
 let mins = document.querySelector(".mins");
 let secs = document.querySelector(".secs");
+
+setInterval(function updateTime() {
+}, 1000);
+
+let date = new Date();
+const next7days = date.getTime() + 7 * 24 * 60 * 60 * 1000; // add next 7 days time
+console.log(new Date(next7days))
