ec56dfb027f56ea5acd1a16c6c3814cafffac5d8
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-13 22:01:07 +0300

    update logic to find days in word

--- 3. Countdown Timer/script.js
+++ 3. Countdown Timer/script.js
@@ -4,9 +4,20 @@ let hours = document.querySelector(".hours");
 let mins = document.querySelector(".mins");
 let secs = document.querySelector(".secs");
 
-setInterval(function updateTime() {
-}, 1000);
+setInterval(function updateTime() {}, 1000);
 
 let date = new Date();
 const next7days = date.getTime() + 7 * 24 * 60 * 60 * 1000; // add next 7 days time
-console.log(new Date(next7days))
+console.log(new Date(next7days));
+
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+console.log(daysOfWeek[date.getDay()]);
