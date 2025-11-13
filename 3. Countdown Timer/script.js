cbc53af8e8a7951d7c986545b577c4763d2a0e10
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-13 21:29:36 +0300

    initial code in script file & update in index file

---
+++ 3. Countdown Timer/script.js
@@ -0,0 +1,5 @@
+let giveAway = document.querySelector("h1");
+let days = document.querySelector(".days");
+let hours = document.querySelector(".hours");
+let mins = document.querySelector(".mins");
+let secs = document.querySelector(".secs");
