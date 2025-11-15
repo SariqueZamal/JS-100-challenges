cf0df9c3b72bcb50d5a1aab1e25064ee380d0ae5
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-15 21:32:39 +0300

    update logic for the days

--- 3. Countdown Timer/script.js
+++ 3. Countdown Timer/script.js
@@ -29,12 +29,11 @@ const monthNames = [
   "December",
 ];
 
-
-// Logic for Give Away Time update 
+// Logic for Give Away Time update
 function updateGiveAwayTime() {
   let date = new Date();
   // Find after 7 days time
-  const after7days = date.getTime() + 24 * 7 * 60 * 60 * 1000;
+  const after7days = date.getTime() + 24 * 27 * 60 * 60 * 1000;
   const giveAwayDate = new Date(after7days);
   let giveAwayday = daysOfWeek[giveAwayDate.getDay()];
   let giveAwaydate = giveAwayDate.getDate();
@@ -46,12 +45,30 @@ function updateGiveAwayTime() {
 
 updateGiveAwayTime(); // Initial Call
 
-
 // Update Time in every second
-function updateTime(){
+function updateTime() {
   let date = new Date();
+  const after7days = date.getTime() + 24 * 27 * 60 * 60 * 1000;
+  const giveAwayDate = new Date(after7days);
 
+  let currDay = daysOfWeek[date.getDay()];
+  let currHour = date.getHours();
+  let currMin = date.getMinutes();
+  let currsec = date.getSeconds();
+
+  let diffInMilliseconds = Math.abs(giveAwayDate.getTime() - date.getTime());
+
+  if (currHour > 11 && currMin > 30) {
+    days.innerText = `${diffInMilliseconds / (1000 * 60 * 60 * 24) - 1}`;
+  } else {
+    days.innerText = `${diffInMilliseconds / (1000 * 60 * 60 * 24)}`;
+  }
+  
+  hours.innerText = `${Math.abs(currHour - 11)}`;
+  mins.innerText = `${Math.abs(30 - currMin)}`;
 }
+
+updateTime();
 // setInterval(updateTime, 1000);
 
 // console.log(date.getHours() - new Date(next7days).getHours());
