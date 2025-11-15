817d42eea58d7d444d963de8f7d048f934919a8d
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-15 21:54:57 +0300

    update logic

--- 3. Countdown Timer/script.js
+++ 3. Countdown Timer/script.js
@@ -33,14 +33,14 @@ const monthNames = [
 function updateGiveAwayTime() {
   let date = new Date();
   // Find after 7 days time
-  const after7days = date.getTime() + 24 * 27 * 60 * 60 * 1000;
+  const after7days = date.getTime() + 24 * 7 * 60 * 60 * 1000;
   const giveAwayDate = new Date(after7days);
-  let giveAwayday = daysOfWeek[giveAwayDate.getDay()];
+  let giveAwayDay = daysOfWeek[giveAwayDate.getDay()];
   let giveAwaydate = giveAwayDate.getDate();
   let giveAwaymonth = monthNames[giveAwayDate.getMonth()];
   let giveAwayYear = giveAwayDate.getFullYear();
 
-  giveAway.innerText = `Giveaway ends on ${giveAwayday}, ${giveAwaydate} ${giveAwaymonth} ${giveAwayYear} 11:30 am`;
+  giveAway.innerText = `Giveaway ends on ${giveAwayDay}, ${giveAwaydate} ${giveAwaymonth} ${giveAwayYear} 11:30 am`;
 }
 
 updateGiveAwayTime(); // Initial Call
@@ -48,7 +48,7 @@ updateGiveAwayTime(); // Initial Call
 // Update Time in every second
 function updateTime() {
   let date = new Date();
-  const after7days = date.getTime() + 24 * 27 * 60 * 60 * 1000;
+  const after7days = date.getTime() + 24 * 7 * 60 * 60 * 1000;
   const giveAwayDate = new Date(after7days);
 
   let currDay = daysOfWeek[date.getDay()];
@@ -58,14 +58,19 @@ function updateTime() {
 
   let diffInMilliseconds = Math.abs(giveAwayDate.getTime() - date.getTime());
 
-  if (currHour > 11 && currMin > 30) {
-    days.innerText = `${diffInMilliseconds / (1000 * 60 * 60 * 24) - 1}`;
-  } else {
-    days.innerText = `${diffInMilliseconds / (1000 * 60 * 60 * 24)}`;
-  }
-  
-  hours.innerText = `${Math.abs(currHour - 11)}`;
-  mins.innerText = `${Math.abs(30 - currMin)}`;
+  // if (currHour > 11 && currMin > 30) {
+  //   days.innerText = `${diffInMilliseconds / (1000 * 60 * 60 * 24) - 1}`;
+  // } else {
+  //   days.innerText = `${diffInMilliseconds / (1000 * 60 * 60 * 24)}`;
+  // }
+
+  // if (currMin >= 30 && currsec > 0) {
+  //   hours.innerText = `${Math.abs(23 - (currHour - 11))}`;
+  // } else {
+  //   hours.innerText = `${Math.abs(24 - (currHour - 11))}`;
+  // }
+  // hours.innerText = `${Math.abs(23 - (currHour - 11))}`;
+  // mins.innerText = `${Math.abs(30 - currMin)}`;
 }
 
 updateTime();
