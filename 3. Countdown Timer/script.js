e246f767f0130fe9619e2de751d5764066079a91
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-15 21:09:37 +0300

    update logic

--- 3. Countdown Timer/script.js
+++ 3. Countdown Timer/script.js
@@ -28,27 +28,31 @@ const monthNames = [
   "November",
   "December",
 ];
-// console.log(daysOfWeek[date.getDay()]);
 
+
+// Logic for Give Away Time update 
 function updateGiveAwayTime() {
   let date = new Date();
   // Find after 7 days time
-  const after7days = date.getTime() + 24 * 18 * 60 * 60 * 1000;
-  const giveAwayDate = new Date(after7days)
+  const after7days = date.getTime() + 24 * 7 * 60 * 60 * 1000;
+  const giveAwayDate = new Date(after7days);
   let giveAwayday = daysOfWeek[giveAwayDate.getDay()];
   let giveAwaydate = giveAwayDate.getDate();
   let giveAwaymonth = monthNames[giveAwayDate.getMonth()];
   let giveAwayYear = giveAwayDate.getFullYear();
-  let currTime = date.getHours();
 
-  giveAway.innerText = `Giveaway ends on ${giveAwayday}, ${giveAwaydate} ${giveAwaymonth} ${giveAwayYear} 11:30am`
+  giveAway.innerText = `Giveaway ends on ${giveAwayday}, ${giveAwaydate} ${giveAwaymonth} ${giveAwayYear} 11:30 am`;
+}
 
-};
+updateGiveAwayTime(); // Initial Call
 
-updateGiveAwayTime();
 
-setInterval(function updateTime() {}, 1000);
+// Update Time in every second
+function updateTime(){
+  let date = new Date();
 
-let date = new Date();
+}
+// setInterval(updateTime, 1000);
+
 // console.log(date.getHours() - new Date(next7days).getHours());
 // console.log(new Date(next7days));
