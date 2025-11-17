7af27ef476777a148f613d2af5454cbf1eb6a75f
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-17 22:53:13 +0300

    update code

--- 4. Age Calculator/app.js
+++ 4. Age Calculator/app.js
@@ -4,20 +4,37 @@ let para = document.querySelector("p");
 
 button.addEventListener("click", function () {
   let currDate = new Date();
+  let age;
 
+    
   if (
     date.valueAsDate == null ||
     date.valueAsDate > currDate ||
     date.valueAsDate.getFullYear() < 1900
   ) {
-    para.innerText = "please put the valid Date.";
+    para.innerText = "please enter the valid date.";
     setTimeout(function () {
       para.innerText = "";
     }, 1000);
     return;
   }
 
-  console.dir(date.valueAsDate);
-  console.dir(date);
-  console.log(date.valueAsDate.getFullYear());
+  if ((date.valueAsDate.getMonth() <= currDate.getMonth()) && (date.valueAsDate.getDate() <= currDate.getDate())) {
+    age = currDate.getFullYear() - date.valueAsDate.getFullYear();
+  } else if ((currDate.getTime() - date.valueAsDate.getTime()) / (1000 * 60 * 60 * 24) < 30) {
+    age = Math.floor((currDate.getTime() - date.valueAsDate.getTime()) / (1000 * 60 * 60 * 24));
+    para.innerText = `You are ${age} days old.`;
+    return;
+  } else if (date.valueAsDate.getFullYear() == currDate.getFullYear()) {
+    age = currDate.getMonth() - date.valueAsDate.getMonth();
+    para.innerText = `You are ${age} months old.`;
+    return;
+  } else {
+    age = (currDate.getFullYear() - date.valueAsDate.getFullYear() )- 1;
+  }
+
+  para.innerText = `You are ${age} years old.`;
+  setTimeout(function () {
+      para.innerText = "";
+    }, 1000);
 });
