c522041fe81e41972736e419a748ee4cd11e185c
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-17 22:04:17 +0300

    update corner case code

--- 4. Age Calculator/app.js
+++ 4. Age Calculator/app.js
@@ -1,2 +1,20 @@
 let date = document.querySelector("#date");
-console.log(date);
+let button = document.querySelector("button");
+let para = document.querySelector("p");
+
+button.addEventListener("click", function () {
+  let currDate = new Date();
+
+  if (
+    date.valueAsDate == null ||
+    date.valueAsDate > currDate ||
+    date.valueAsDate.getFullYear() < 1900
+  ) {
+    para.innerText = "please put the valid Date.";
+    return;
+  }
+
+  console.dir(date.valueAsDate);
+  console.dir(date);
+  console.log(date.valueAsDate.getFullYear());
+});
