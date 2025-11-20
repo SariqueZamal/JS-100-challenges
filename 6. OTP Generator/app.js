90482ca014ae655a5dc26eb36d1ed63dee4d3e7b
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-20 22:53:06 +0300

    upddate final code

--- 6. OTP Generator/app.js
+++ 6. OTP Generator/app.js
@@ -1,1 +1,33 @@
-let mobile = document.querySelector("#mobile")
+let mobile = document.querySelector("#mobile");
+let button = document.querySelector("button");
+let para = document.querySelector("p");
+
+button.addEventListener("click", function () {
+  let trimVal = mobile.value.trim();
+  if (trimVal === "" || trimVal.length < 10 || trimVal.length > 10) {
+    para.innerText = "Please enter valid 10 digits mobile Number";
+    setTimeout(() => {
+      para.innerText = "";
+    }, 1000);
+    return;
+  }
+  let mob = Number(mobile.value);
+
+  if (mob === NaN) {
+    para.innerText = "Please enter valid 10 digits mobile Number";
+    setTimeout(() => {
+      para.innerText = "";
+    }, 1000);
+    return;
+  }
+
+  let otp = "";
+  for (let i = 0; i < 6; i++) {
+    otp += Math.floor(Math.random() * 10);
+  }
+
+  para.innerText = otp;
+  para.style.color = "violet";
+
+  mobile.value = "";
+});
