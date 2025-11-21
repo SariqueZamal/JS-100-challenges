46d22aa5cb649931097f3519daa2c6bbfc87415a
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-21 20:51:56 +0300

    update code

--- 7. Multiplication Table Generator/app.js
+++ 7. Multiplication Table Generator/app.js
@@ -4,9 +4,19 @@ let para = document.querySelector("p");
 
 button.addEventListener("click", function () {
   let num = Number(input.value);
-    
-  if (num == NaN || num == 0) {
+  if (num != Number || num == 0) {
     para.innerText = "Please enter a Valid Number";
+    input.value = "";
+    setInterval(() => {
+      para.innerText = "";
+    }, 1000);
     return;
   }
+  //   } else {
+  //     for (let i = 1; i <= 10; i++) {
+  //       para.innerText += `${num}  x  ${i}  =  ${num * i}
+
+  //       `;
+  //     }
+  //   }
 });
