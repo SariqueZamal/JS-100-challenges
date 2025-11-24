9e546a5c8c9c131998d032105ab6755553625ed7
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-24 21:44:34 +0300

    update code

--- 10. Marks Percentage Calculator/app.js
+++ 10. Marks Percentage Calculator/app.js
@@ -3,17 +3,31 @@ let totalMark = document.querySelector("#totalMark");
 let result = document.querySelector("p");
 let button = document.querySelector("button");
 
-button.addEventListener("click", ()=>{
-    let markSec = Number(markSecured.value);
-    let total = Number(totalMark.value);
+button.addEventListener("click", () => {
+  let markSec = Number(markSecured.value);
+  let total = Number(totalMark.value);
 
-    if(Number.isNaN(markSec) || Number.isNaN(total) || markSec == 0 || total == 0){
-        result.innerText = "Please enter a valid Number."
-    
-        setTimeout(() => {
-            result.style.color = "red";
-            result.innerText = "";
-        }, 1000);
-        return;
-    }
-})
+  if (
+    Number.isNaN(markSec) ||
+    Number.isNaN(total) ||
+    markSec == 0 ||
+    total == 0
+  ) {
+    result.innerText = "Please enter a valid Number.";
+
+    setTimeout(() => {
+      result.style.color = "red";
+      result.innerText = "";
+    }, 1000);
+    return;
+  }
+
+  let percentage = (100 / total) * markSec;
+  result.style.color = "green";
+  result.innerText = percentage.toFixed(2);
+
+  setTimeout(() => {
+    result.innerText = "";
+  }, 2000);
+  return;
+});
