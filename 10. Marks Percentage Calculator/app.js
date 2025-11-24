d389502f2560a8d58a4153fc9db1e19e746becdd
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-24 21:34:20 +0300

    update js

--- 10. Marks Percentage Calculator/app.js
+++ 10. Marks Percentage Calculator/app.js
@@ -1,3 +1,13 @@
 let markSecured = document.querySelector("#markSec");
 let totalMark = document.querySelector("#totalMark");
+let result = document.querySelector("p");
 let button = document.querySelector("button");
+
+button.addEventListener("click", ()=>{
+    let markSec = Number(markSecured);
+    let total = Number(totalMark);
+
+    if(Number.isNaN(markSec) || Number.isNaN(total) || markSec == 0 || total == 0){
+
+    }
+})
