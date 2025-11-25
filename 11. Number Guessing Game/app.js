007cdc2082b591bd24015f068a199977a38e1b59
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-25 21:50:19 +0300

    update js''

--- 11. Number Guessing Game/app.js
+++ 11. Number Guessing Game/app.js
@@ -1,3 +1,13 @@
 let input = document.querySelector("input");
 let button = document.querySelector("button");
 let result = document.querySelector("p");
+
+button.addEventListener("click", ()=>{
+    let number = Number(input.value);
+    if(Number.isNaN(number) || number == 0){
+        result.innerText = "Please Enter a valid Number";
+        setTimeout(() => {
+            result.innerText = "";
+        }, 1000);
+    }
+})
