c280ecacc368d1b7d7a61cd922d97c3f7e7ec01f
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-26 20:59:15 +0300

    update code

--- 11. Number Guessing Game/app.js
+++ 11. Number Guessing Game/app.js
@@ -4,9 +4,12 @@ let result = document.querySelector("p");
 
 button.addEventListener("click", ()=>{
     let number = Number(input.value);
+
     if(Number.isNaN(number) || number == 0){
         result.style.color = "red";
         result.innerText = "Please Enter a valid Number";
+        input.value = "";
+
         setTimeout(() => {
             result.innerText = "";
         }, 1000);
@@ -14,5 +17,16 @@ button.addEventListener("click", ()=>{
     }
 
     let random = Math.floor(Math.random() * 100) 
+
+    if(number == random){
+        result.style.color = "green";
+        result.innerText = "You Entered the correct answer";
+        return;
+    }else{
+        result.style.color = "blue";
+        result.innerText = `You Entered the wrong anwer.
+        Correct ans is ${random};
+        `
+    }
     
 })
