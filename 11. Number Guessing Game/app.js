6ea436cd369b4af69b7a3d41e50be4ed6dafbe17
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-26 21:04:54 +0300

    update comment

--- 11. Number Guessing Game/app.js
+++ 11. Number Guessing Game/app.js
@@ -3,8 +3,10 @@ let button = document.querySelector("button");
 let result = document.querySelector("p");
 
 button.addEventListener("click", () => {
+  // Convert text into Number.
   let number = Number(input.value);
 
+  // Checking if Not a Number & number is zero
   if (Number.isNaN(number) || number == 0) {
     result.style.color = "red";
     result.innerText = "Please Enter a valid Number";
@@ -16,8 +18,10 @@ button.addEventListener("click", () => {
     return;
   }
 
+  // Generating random number between 1 to 100.
   let random = Math.floor(Math.random() * 100);
 
+  // Check the random number with the user input.
   if (number == random) {
     input.value = "";
     result.style.color = "green";
