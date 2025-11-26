29f275c7adf97ac3cb7ccc0e029a81934e71060c
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-26 21:02:28 +0300

    formatting

--- 11. Number Guessing Game/app.js
+++ 11. Number Guessing Game/app.js
@@ -2,32 +2,31 @@ let input = document.querySelector("input");
 let button = document.querySelector("button");
 let result = document.querySelector("p");
 
-button.addEventListener("click", ()=>{
-    let number = Number(input.value);
+button.addEventListener("click", () => {
+  let number = Number(input.value);
 
-    if(Number.isNaN(number) || number == 0){
-        result.style.color = "red";
-        result.innerText = "Please Enter a valid Number";
-        input.value = "";
+  if (Number.isNaN(number) || number == 0) {
+    result.style.color = "red";
+    result.innerText = "Please Enter a valid Number";
+    input.value = "";
 
-        setTimeout(() => {
-            result.innerText = "";
-        }, 1000);
-        return;
-    }
+    setTimeout(() => {
+      result.innerText = "";
+    }, 1000);
+    return;
+  }
 
-    let random = Math.floor(Math.random() * 100) 
+  let random = Math.floor(Math.random() * 100);
 
-    if(number == random){
-        input.value = "";
-        result.style.color = "green";
-        result.innerText = "You Entered the correct answer";
-        return;
-    }else{
-        input.value = "";
-        result.style.color = "blue";
-        result.innerText = `You entered the wrong anwer.
-        Correct ans is ${random}.`
-    }
-    
-})
+  if (number == random) {
+    input.value = "";
+    result.style.color = "green";
+    result.innerText = "You Entered the correct answer";
+    return;
+  } else {
+    input.value = "";
+    result.style.color = "blue";
+    result.innerText = `You entered the wrong anwer.
+        Correct ans is ${random}.`;
+  }
+});
