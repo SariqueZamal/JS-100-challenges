8b697f24d8e5cb9d9a03cfa8f601e8c0d7fcc5e6
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-29 21:09:35 +0300

    insert button
    insert reset button and some style

--- 12. Stopwatch/app.js
+++ 12. Stopwatch/app.js
@@ -1,10 +1,14 @@
 let result = document.querySelector("p");
 let start = document.querySelector("#start");
+let reset = document.querySelector("#reset");
 
 let min = "00";
 let sec = "00";
-let milliSec = "00";
+let milliSec = 0;
 
 start.addEventListener("click",()=>{
-    
+    setInterval(()=>{
+        milliSec++;
+        result.innerText =
+    }, 100)
 })
