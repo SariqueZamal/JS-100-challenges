73e26715fc8b11e6875f0658e251237dbc5c2320
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-29 21:32:13 +0300

    stopwatch code
    code update for milli seconds & seconds

--- 12. Stopwatch/app.js
+++ 12. Stopwatch/app.js
@@ -2,13 +2,26 @@ let result = document.querySelector("p");
 let start = document.querySelector("#start");
 let reset = document.querySelector("#reset");
 
-let min = "00";
-let sec = "00";
+let min = "0"+0;
+let sec = "0"+0;
 let milliSec = 0;
 
-start.addEventListener("click",()=>{
-    setInterval(()=>{
-        milliSec++;
-        result.innerText =
-    }, 100)
-})
+start.addEventListener("click", () => {
+  setInterval(() => {
+    milliSec++;
+    if (milliSec < 10) {
+      milliSec = "0" + milliSec;
+    } 
+    else if(milliSec == 100){
+        sec++;
+        if(sec < 10){
+            sec = "0" + sec;
+        }
+        milliSec = milliSec - 100;
+    }
+    result.innerText = `${min}:${sec}:${milliSec}`;
+    //   if (milliSec < 100) {
+    //   } else if ((milliSec >= 100) & (sec < 60)) {
+    //   }
+  }, 10);
+});
