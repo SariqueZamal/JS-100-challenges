bfde6dd53431d625e5db01cc14ecd37327bf9e54
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-30 16:39:11 +0300

    update stop watch code
    update the code for when we click on the stop button

--- 12. Stopwatch/app.js
+++ 12. Stopwatch/app.js
@@ -8,37 +8,38 @@ let min = "0" + 0;
 let sec = "0" + 0;
 let milliSec = 0;
 
+let updateTime;
+
 startStop.addEventListener("click", () => {
   if (!isRunning) {
     isRunning = true;
     startStop.innerText = "Stop";
     reset.style.visibility = "visible";
     lap.style.visibility = "visible";
-  setInterval(function updateTime(){
-      milliSec++;
-      if (milliSec < 10) {
-        milliSec = "0" + milliSec;
-      } else if (milliSec == 100) {
-        sec++;
-        if (sec < 10) {
-          sec = "0" + sec;
+      updateTime = setInterval(() => {
+        milliSec++;
+        if (milliSec < 10) {
+          milliSec = "0" + milliSec;
+        } else if (milliSec == 100) {
+          sec++;
+          if (sec < 10) {
+            sec = "0" + sec;
+          }
+          milliSec = milliSec - 100;
+        } else if (sec == 60) {
+          min++;
+          if (min < 10) {
+            min = "0" + min;
+          }
+          sec = sec - 60;
         }
-        milliSec = milliSec - 100;
-      } else if (sec == 60) {
-        min++;
-        if (min < 10) {
-          min = "0" + min;
-        }
-        sec = sec - 60;
-      }
-      result.innerText = `${min}:${sec}:${milliSec}`;
-    }, 10);
-    } else {
-      isRunning = false;
-      startStop.innerText = "Start";
-      lap.style.visibility = "hidden";
-      clearInterval(updateTime, 1000);
-      result.innerText = `${min}:${sec}:${milliSec}`;
-      return;
-    }
+        result.innerText = `${min}:${sec}:${milliSec}`;
+      }, 10);
+  } else {
+    isRunning = false;
+    startStop.innerText = "Start";
+    lap.style.visibility = "hidden";
+    clearInterval(updateTime, 1000);
+    return;
+  }
 });
