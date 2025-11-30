76ef7efb46ccb91aa7d17474b06ee33456db03d0
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-30 21:31:04 +0300

    initial code for lap

--- 12. Stopwatch/app.js
+++ 12. Stopwatch/app.js
@@ -4,12 +4,19 @@ let reset = document.querySelector("#reset");
 let lap = document.querySelector("#lap");
 let lapContainer = document.querySelector(".lapContainer");
 let isRunning = false;
+let isLap = false;
 
 let min = "0" + 0;
 let sec = "0" + 0;
 let milliSec = 0;
 
 let updateTime;
+
+let lapM = "0" + 0;
+let lapS = "0" + 0;
+let lapMilli = "0" + 0;
+
+let lapUpdateTime;
 
 startStop.addEventListener("click", () => {
   if (!isRunning) {
@@ -35,12 +42,20 @@ startStop.addEventListener("click", () => {
         sec = sec - 60;
       }
       result.innerText = `${min}:${sec}.${milliSec}`;
+      if (isLap) {
+        // lapUpdateTime();
+        lapContainer.childNodes[lapCount - 1].innerHTML = `${lapCount} </br>
+      ${lapM}:${lapS}.${lapMilli}
+      ${min}:${sec}.${milliSec}
+      `;
+      }
     }, 10);
   } else {
     isRunning = false;
     startStop.innerText = "Start";
     lap.style.visibility = "hidden";
     clearInterval(updateTime, 1000);
+    clearInterval(lapUpdateTime, 1000);
     return;
   }
 });
@@ -52,25 +67,63 @@ reset.addEventListener("click", () => {
   startStop.innerText = "Start";
   lap.style.visibility = "hidden";
   clearInterval(updateTime, 1000);
+  clearInterval(lapUpdateTime, 1000);
+  isLap = false;
   return;
 });
 
-
 let lapCount = 0;
+
 lap.addEventListener("click", () => {
+  isLap = true;
   if (lapCount === 0) {
-    lapCount = 2;
+    lapCount++;
     let lapContent1 = document.createElement("div");
     lapContent1.classList.add("lapContent");
-    let lapContent2 = document.createElement("div");
+
     let lapMin = min;
     let lapSec = sec;
     let lapMilliSec = milliSec;
 
-    lapContent1.innerHTML =`1 </br>
+    lapContent1.innerHTML = `1 </br>
     ${lapMin}:${lapSec}.${lapMilliSec}</br>
     ${lapMin}:${lapSec}.${lapMilliSec}
     `;
     lapContainer.append(lapContent1);
+
+    lapCount++;
+    let lapContent2 = document.createElement("div");
+    lapContent2.classList.add("lapContent");
+    lapContent2.id = `${lapCount}`;
+
+    lapUpdateTime = setInterval(() => {
+      lapMilli++;
+      if (lapMilli < 10) {
+        lapMilli = "0" + lapMilli;
+      } else if (lapMilli == 100) {
+        lapS++;
+        if (lapS < 10) {
+          lapS = "0" + lapS;
+        }
+        lapMilli = lapMilli - 100;
+      } else if (lapS == 60) {
+        lapM++;
+        if (lapM < 10) {
+          lapM = "0" + lapM;
+        }
+        lapS = lapS - 60;
+      }
+      lapContent2.innerHTML = `${lapCount} </br>
+      ${lapM}:${lapS}.${lapMilli}
+      ${min}:${sec}.${milliSec}
+      `;
+    }, 10);
+
+    lapContainer.append(lapContent2);
+  } else {
+    lapContainer.childNodes[lapCount - 1];
+    lapCount++;
+    let lapContent = document.createElement("div");
+    lapContent.classList.add("lapContent");
   }
 });
