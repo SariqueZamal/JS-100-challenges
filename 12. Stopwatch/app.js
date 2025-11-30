0274c920cb4d5802c1f3acf6ee2e1b2aff73f925
Sarique Zamal <sariquezamal0@gmail.com>
2025-11-30 12:59:01 +0300

    remove stop button
    as i plan to toggle start & stop in one button

--- 12. Stopwatch/app.js
+++ 12. Stopwatch/app.js
@@ -2,40 +2,41 @@ let result = document.querySelector("p");
 let start = document.querySelector("#start");
 let reset = document.querySelector("#reset");
 let stop = document.querySelector("#stop");
-let lap = document.querySelector("#lap")
-// let isRunning = false;
+let lap = document.querySelector("#lap");
+let isRunning = false;
 
 let min = "0" + 0;
 let sec = "0" + 0;
 let milliSec = 0;
 
 start.addEventListener("click", () => {
-start.style.visibility =  "hidden";
-stop.style.visibility = "visible";
-  setInterval(() => {
-    milliSec++;
-    if (milliSec < 10) {
-      milliSec = "0" + milliSec;
-    } 
-    else if(milliSec == 100){
+  if (!isRunning) {
+    isRunning = true;
+    start.style.visibility = "hidden";
+    stop.style.visibility = "visible";
+    setInterval(() => {
+      milliSec++;
+      if (milliSec < 10) {
+        milliSec = "0" + milliSec;
+      } else if (milliSec == 100) {
         sec++;
-        if(sec < 10){
-            sec = "0" + sec;
+        if (sec < 10) {
+          sec = "0" + sec;
         }
         milliSec = milliSec - 100;
-    }
-    else if(sec == 60){
+      } else if (sec == 60) {
         min++;
-        if(min < 10){
-            min = "0" + min;
+        if (min < 10) {
+          min = "0" + min;
         }
         sec = sec - 60;
-    }
-    result.innerText = `${min}:${sec}:${milliSec}`;
-  }, 10);
+      }
+      result.innerText = `${min}:${sec}:${milliSec}`;
+    }, 10);
+  }
 });
 
-stop.addEventListener("click", ()=>{
-    stop.style.visibility = "hidden";
-    start.style.visibility = "visible";
-})
+stop.addEventListener("click", () => {
+  stop.style.visibility = "hidden";
+  start.style.visibility = "visible";
+});
