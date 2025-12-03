259b5053e737bafa671fb635a45295cf7e37219b
Sarique Zamal <sariquezamal0@gmail.com>
2025-12-03 22:10:31 +0300

    update initial code

--- 13. Random Dog Image/app.js
+++ 13. Random Dog Image/app.js
@@ -1,2 +1,15 @@
 let image = document.querySelector("img");
 let button = document.querySelector("button");
+
+button.addEventListener("click", ()=>{
+let xhr = new XMLHttpRequest();
+
+xhr.responseType = "JSON";
+xhr.onload = ()=>{
+
+}
+
+xhr.open('GET',"");
+xhr.send();
+
+})
