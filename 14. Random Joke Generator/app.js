8727a76c745739e1aaf8f9f2cca2ff81454600e0
Sarique Zamal <sariquezamal0@gmail.com>
2025-12-04 21:45:23 +0300

    update final code

--- 14. Random Joke Generator/app.js
+++ 14. Random Joke Generator/app.js
@@ -1,2 +1,17 @@
 let button = document.querySelector("button");
 let result = document.querySelector("p");
+
+
+button.addEventListener("click", ()=>{
+    let xhr = new XMLHttpRequest();
+
+    xhr.responseType = "json";
+
+    xhr.onload = ()=>{
+        result.innerText = xhr.response.setup;
+        console.log(xhr.response);
+    }
+
+    xhr.open("GET", "https://v2.jokeapi.dev/joke/Any")
+    xhr.send();
+})
