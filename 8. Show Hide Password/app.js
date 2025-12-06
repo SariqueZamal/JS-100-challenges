a109af432f3f36a1b6833e4120f8b28efb8ffd08
Sarique Zamal <sariquezamal0@gmail.com>
2025-12-06 21:20:06 +0300

    update code

--- 8. Show Hide Password/app.js
+++ 8. Show Hide Password/app.js
@@ -2,18 +2,19 @@ let input = document.querySelector("input");
 let button = document.querySelector("button");
 let para = document.querySelector("p");
 
-button.addEventListener("click", ()=>{
-    if(input.value.trim() ==""){
-        para.style.color = "red";
-        para.innerText = "Please enter the Password";
-        return;
-    }
-    else if(input.type == "password"){
-        input.type = "text";
-        button.innerText = "Hide Password";
-    }
-    else{
-        input.type = "password";
-        button.innerText = "Show Password";
-    }
-})
+button.addEventListener("click", () => {
+  if (input.value.trim() == "") {
+    para.style.color = "red";
+    para.innerText = "Please enter the Password";
+    setTimeout(() => {
+      para.innerText = "";
+    }, 1000);
+    return;
+  } else if (input.type == "password") {
+    input.type = "text";
+    button.innerText = "Hide Password";
+  } else {
+    input.type = "password";
+    button.innerText = "Show Password";
+  }
+});
