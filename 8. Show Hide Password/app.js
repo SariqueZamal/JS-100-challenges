a077dec8becd274c2b71af4e10c7406db21bbb6b
Sarique Zamal <sariquezamal0@gmail.com>
2025-12-06 21:18:09 +0300

    update code for corner case

--- 8. Show Hide Password/app.js
+++ 8. Show Hide Password/app.js
@@ -1,9 +1,12 @@
 let input = document.querySelector("input");
 let button = document.querySelector("button");
+let para = document.querySelector("p");
 
 button.addEventListener("click", ()=>{
     if(input.value.trim() ==""){
-        
+        para.style.color = "red";
+        para.innerText = "Please enter the Password";
+        return;
     }
     else if(input.type == "password"){
         input.type = "text";
