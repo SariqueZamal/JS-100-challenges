1b0929fcd1da351c58754c0a61212635e4117fde
Sarique Zamal <sariquezamal0@gmail.com>
2025-12-08 20:52:16 +0300

    update js
    update code for if no word enter

--- 15. Dictionary App/app.js
+++ 15. Dictionary App/app.js
@@ -2,9 +2,13 @@ let input = document.querySelector("input");
 let button = document.querySelector("button");
 let result = document.querySelector("#result");
 
-button.addEventListener("click", ()=>{
-    if(input.value.trim() === ""){
-        result.innerHTML = `<p id="error">Please enter a word.</p>`;
-        return;
-    }
-})
+button.addEventListener("click", () => {
+  let word = input.value.trim();
+  if (word === "") {
+    result.innerHTML = `<p id="error">Please enter a word.</p>`;
+    setTimeout(() => {
+      result.innerHTML = "";
+    }, 1000);
+    return;
+  }
+});
