1a3724a75741c5bc8159c7dc61323ac831df3295
Sarique Zamal <sariquezamal0@gmail.com>
2025-12-08 20:45:47 +0300

    update js
    update code for corner case

--- 15. Dictionary App/app.js
+++ 15. Dictionary App/app.js
@@ -1,3 +1,10 @@
 let input = document.querySelector("input");
 let button = document.querySelector("button");
 let result = document.querySelector("#result");
+
+button.addEventListener("click", ()=>{
+    if(input.value.trim() === ""){
+        result.innerHTML = `<p id="error">Please enter a word.</p>`;
+        return;
+    }
+})
