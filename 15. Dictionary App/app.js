b3faabb2b0119df264fc581bfa908a6383d3a3dc
Sarique Zamal <sariquezamal0@gmail.com>
2025-12-09 20:25:38 +0300

    update word & defination code

--- 15. Dictionary App/app.js
+++ 15. Dictionary App/app.js
@@ -18,18 +18,22 @@ function getMeaning(word) {
   let xhr = new XMLHttpRequest();
   xhr.responseType = "json";
 
-  result.innerHTML =`Loading. . .`;
+  result.innerHTML = `Loading. . .`;
 
   xhr.onload = () => {
     if (xhr.response.title == "No Definitions Found") {
       result.innerHTML = `<p id="error">Sorry Dude, we couldn't find definitions for the word you were looking for.</p>`;
       return;
     }
-     
+
+    result.innerHTML = `<h4>Word: ${xhr.response[0].word}</h4><br>
+    <h4>Meaning:</h4>
+    <p>${xhr.response[0].meanings[0].definitions[0].definition}</p>
+    `;
+    
     console.log(xhr.response[0]);
     console.log(xhr.response[0].word);
     console.log(xhr.response[0].meanings[0].definitions[0].definition);
-
   };
 
   xhr.open("GET", `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
