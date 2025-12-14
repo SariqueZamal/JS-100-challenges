708b9c6ca041a9dd91577c3c8d8592204dce67ec
Sarique Zamal <sariquezamal0@gmail.com>
2025-12-14 21:51:55 +0300

    update final code for code

--- 15. Word Counter/app.js
+++ 15. Word Counter/app.js
@@ -1,8 +1,19 @@
 let textArea = document.querySelector("textarea");
 let result = document.querySelector("p");
 
-textArea.addEventListener("keypress", ()=> {
-   let count = textArea.value.length;
-   result.innerText = `Total word count is ${count}`; 
+textArea.addEventListener("keypress", (event) => {
+  let count = textArea.value.length;
+  result.innerText = `Total word count is ${count}`;
 });
 
+textArea.addEventListener("keydown", (event) => {
+  let count;
+  if (event.key === "Backspace") {
+    count = textArea.value.length;
+  } else if (event.key === "Delete") {
+    count = textArea.value.length;
+  } else {
+    count = textArea.value.length;
+  }
+  result.innerText = `Total word count is ${count}`;
+});
