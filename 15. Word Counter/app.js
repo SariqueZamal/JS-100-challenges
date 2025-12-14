28759303de2f11f56a3b242be648fbc0cf92b795
Sarique Zamal <sariquezamal0@gmail.com>
2025-12-14 21:41:37 +0300

    update code

--- 15. Word Counter/app.js
+++ 15. Word Counter/app.js
@@ -1,3 +1,8 @@
 let textArea = document.querySelector("textarea");
 let result = document.querySelector("p");
 
+textArea.addEventListener("keypress", ()=> {
+   let count = textArea.value.length;
+   result.innerText = `Total word count is ${count}`; 
+});
+
