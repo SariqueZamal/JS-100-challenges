2e68afc31123f93670ee9120a3619192ee673374
Sarique Zamal <sariquezamal0@gmail.com>
2025-12-17 21:03:00 +0300

    update code

--- 16. Tip Calculator/app.js
+++ 16. Tip Calculator/app.js
@@ -3,3 +3,10 @@ let people = document.querySelector("#people");
 let button = document.querySelector("button");
 let result = document.querySelector("p");
 
+button.addEventListener("click", ()=>{
+    let expense = expenses;
+    let person  = people;
+
+    console.dir(expense)
+    console.dir(person)
+})
