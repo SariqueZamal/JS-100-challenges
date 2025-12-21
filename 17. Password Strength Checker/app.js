4457b06379e210f466c8d403a2313dc084b2b04f
Sarique Zamal <sariquezamal0@gmail.com>
2025-12-21 21:09:21 +0300

    update code

--- 17. Password Strength Checker/app.js
+++ 17. Password Strength Checker/app.js
@@ -1,6 +1,12 @@
 let password = document.querySelector("input");
-let passwordStrength = document.querySelector("div");
+let passwordStrength = document.querySelector("p");
 let button = document.querySelector("button");
+
+
+let uppercase = false;
+let lowercase = false;
+let number = false;
+let specialChar = false;
 
 button.addEventListener("click", ()=>{
 
