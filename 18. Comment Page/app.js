7c44cceeff70bcdb295a8385fb408ea7ed3e637f
Sarique Zamal <sariquezamal0@gmail.com>
2025-12-28 22:52:41 +0300

    update code:

--- 18. Comment Page/app.js
+++ 18. Comment Page/app.js
@@ -1,2 +1,4 @@
 let like = document.querySelector("#like");
 let comment = document.querySelector("#comment");
+let repost = document.querySelector("#repost");
+let send = document.querySelector("#send");
